8d2477699aaf3f208d81248f53d70230c19d578a
ananyacbinayancvn8b-byte <ananyacbinayancvn8b@gmail.com>
2026-04-22 12:08:57 +0530

    intial commit

---
+++ oopconcepts/students.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+students=["Arun","alice","Akhil","Bob"]
+marks=[56,89,43,99] 
+plt.plot(students,marks,marker='o')
+
+plt.xlabel("X-Axis")
+plt.ylabel("Y-Axis")
+
+
+plt.grid()
+
+plt.show()
