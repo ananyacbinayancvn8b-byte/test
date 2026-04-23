10f1d7cb1bdfc22455d88962b6e036cb54180ffa
ananyacbinayancvn8b-byte <ananyacbinayancvn8b@gmail.com>
2026-04-23 10:01:03 +0530

    updated

--- oopconcepts/pandaa.py
+++ oopconcepts/pandaa.py
@@ -1,11 +1,12 @@
-import pandas
+# import pandas
 
-mydataset={
-    "cars":[201"bmw","volvo","ford"],
-    "passings":[3,7,2]
-    }
+# mydataset={
+#     "cars":[201"bmw","volvo","ford"],
+#     "passings":[3,7,2]
+#     }
 
-myvar=pandas.DataFrame(mydataset)
+# myvar=pandas.DataFrame(mydataset)
 
-print(myvar)
+# print(myvar)
 
+# 
